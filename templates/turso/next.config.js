08ee6d0e887d60f4fef8cb990d009352b4cf3fa4
ictx <me@exectx.run>
2024-07-24 01:44:03 -0500

    feat: add turso template

---
+++ templates/turso/next.config.js
@@ -0,0 +1,19 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
+// (when running the application with `next dev`), for more information see:
+// https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {};
+
+export default config;
