3bab4f2f4fb7c8205d9a707bb7073fc43fe420dd
exectx <81214874+exectx@users.noreply.github.com>
2024-07-23 23:42:36 -0500

    fix: add libsql lib for local drizzle-kit ops

--- web/drizzle.local.config.ts
+++ web/drizzle.local.config.ts
@@ -2,6 +2,10 @@ import { type Config } from "drizzle-kit";
 
 import { env } from "@/env";
 
+// This is experimental.
+// If this error is thrown, it means that LOCAL_DB_PATH was not set by `with-local-db` script
+// For instance: You won't be able to use `db:push:local` and `db:migrate:local` scripts
+// If this fails for you, just use `db:generate` and `d1:migrate:local` scripts
 if (!env.LOCAL_DB_PATH) {
   throw new Error("LOCAL_DB_PATH is required, path to local sqlite database");
 }
@@ -10,7 +14,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: env.LOCAL_DB_PATH,
+    url: `file:${env.LOCAL_DB_PATH}`,
   },
   tablesFilter: [`t3-cloudflare_*`],
 } satisfies Config;
