a548951dea6eb9b51ff649a802ec9f157cf5dfbb
ictx <me@exectx.run>
2024-07-24 12:21:42 -0500

    feat: add ratelimit to demo t3-app

--- web/src/server/api/trpc.ts
+++ web/src/server/api/trpc.ts
@@ -11,6 +11,7 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 import { db } from "@/server/db";
+import { unkey } from "@/server/utils/ratelimit";
 
 /**
  * 1. CONTEXT
@@ -27,6 +28,7 @@ import { db } from "@/server/db";
 export const createTRPCContext = async (opts: { headers: Headers }) => {
   return {
     db: db(),
+    ratelimit: unkey(),
     ...opts,
   };
 };
